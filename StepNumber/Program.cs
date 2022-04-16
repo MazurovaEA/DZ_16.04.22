3fdd12837d5c2980538ce87304bd5a51d28a1381
Katerina <ealmazurova@internet.ru>
2022-04-16 13:27:12 +0300

    является ли число степенью двойки

---
+++ StepNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 1: Определите, является ли число степенью двойки: N = 16 -> "Является степень двойки" 
+N = 12 -> “Не является степенью двойки”*/
+int Promt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void Del(int N)
+{
+    if (N / 2 == 0)
+    {
+        Console.WriteLine("Является степенью двойки");
+        return;
+    }
+    if (N % 2 == 0)
+    {
+        Del(N / 2);
+    }
+    else { Console.WriteLine("Не является степенью двойки"); }
+
+}
+
+int N = Promt("Введите N => ");
+Del(N);
